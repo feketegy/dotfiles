4dab6c8cd98ac11340f6a6c394957f1f3b82a16e
George Fekete <george@primalskill.com>
2024-06-26 14:30:08 +0300

    Moved omp config to separate file

--- install.sh
+++ install.sh
@@ -11,17 +11,15 @@ source $DOTFILES/.macos
 source $DOTFILES/install/brew.sh
 source $DOTFILES/install/omz.sh
 source $DOTFILES/install/omp.sh
-
 source $DOTFILES/install/tmux.sh
 source $DOTFILES/install/wezterm.sh
 # ----------
 
-# --- Setup the symlinks ---
+# --- Setup the zshrc ---
 if [ -e $HOME/.zshrc ]
 then
   mv $HOME/.zshrc $HOME/.zshrc.pre-dotfiles
 fi
 
 ln -sf $DOTFILES/.zshrc $HOME/.zshrc
-ln -sfh $DOTFILES/config/oh-my-posh $HOME/.config/oh-my-posh
 # ----------
--- install/omp.sh
+++ install/omp.sh
@@ -4,3 +4,6 @@ if ! command -v oh-my-posh &> /dev/null
 then
   brew install jandedobbeleer/oh-my-posh/oh-my-posh
 fi
+
+# setup symlink to configs
+ln -sfh $DOTFILES/config/oh-my-posh $HOME/.config/oh-my-posh
