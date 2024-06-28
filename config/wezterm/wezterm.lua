f305c5ef82b106f4f77f9479db7beb455e098cba
George Fekete <george@primalskill.com>
2024-06-28 15:35:48 +0300

    Wezterm config update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,9 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Override the Tokyo Night color scheme
 local tokyo_night = wezterm.color.get_builtin_schemes()['Tokyo Night']
-tokyo_night.background = "#0e0e0e"
+tokyo_night.background = "#010101"
 
 config.color_schemes = {
   ['George Tokyo Night'] = tokyo_night
@@ -20,7 +19,6 @@ config.bold_brightens_ansi_colors = true
 config.cell_width = 1.1
 config.line_height = 1.5
 
-
 -- Terminal features
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
@@ -29,6 +27,18 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 8
 
+-- Key bindings
+local act = wezterm.action
 
+config.keys = {
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.Multiple {
+      act.ClearScrollback 'ScrollbackOnly',
+      act.SendKey { key = 'L', mods = 'CTRL' },
+    }    
+  }
+}
 
 return config
