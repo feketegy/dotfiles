5a729088d3d578b39d1ecb1b59812e2e5fc596ca
George Fekete <george@primalskill.com>
2024-12-27 13:41:38 +0200

    Wezterm config update colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,24 +2,33 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Custom color scheme
-config.colors = {
-  background = "#000000",
-  foreground = "#ffa028",
-  cursor_bg = "#ffa028",
-  cursor_border = "#ffa028",
-  cursor_fg = "#000000",
-  selection_bg = "#ffa028",
-  selection_fg = "#000000",
+-- config.colors = {
+--   background = "#000000",
+--   foreground = "#ffa028",
+--   cursor_bg = "#ffa028",
+--   cursor_border = "#ffa028",
+--   cursor_fg = "#000000",
+--   selection_bg = "#ffa028",
+--   selection_fg = "#000000",
 
-  ansi = {"#000000","#FF3428","#f6fe11","#c2a86c","#7a4f17","#7a4f17","#ffa028","#ffa028"},
-  brights = {"#666666","#feba11","#777777","#feba11","#feba11","#feba11","#ffa028","#ffa028"}
+--   ansi = {"#000000","#FF3428","#f6fe11","#c2a86c","#7a4f17","#7a4f17","#ffa028","#ffa028"},
+--   brights = {"#666666","#feba11","#777777","#feba11","#feba11","#feba11","#ffa028","#ffa028"}
+-- }
+
+local theme = wezterm.color.get_builtin_schemes()['Tokyo Night']
+theme.background = '#000000'
+
+config.color_schemes = {
+  ['Modified Tokyo Night'] = theme 
 }
+
+config.color_scheme = 'Modified Tokyo Night'
 
 -- Font
 config.font = wezterm.font("Mononoki Nerd Font Mono")
-config.font_size = 13
+config.font_size = 14
 config.bold_brightens_ansi_colors = true
-config.cell_width = 1.1
+config.cell_width = 1.0
 config.line_height = 1.5
 
 -- Terminal features
@@ -30,6 +39,8 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 1
 
 config.window_close_confirmation = 'NeverPrompt'
+
+config.default_cursor_style = 'BlinkingBar'
 
 -- Key bindings
 local act = wezterm.action
@@ -42,7 +53,23 @@ config.keys = {
       act.ClearScrollback 'ScrollbackOnly',
       act.SendKey { key = 'L', mods = 'CTRL' },
     }    
-  }
+  },
+  {
+    key = '|',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = '~',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      size = { Percent = 50 },
+    },
+  },
 }
 
 return config
