bc9819d951bcd79210c92f06d715baa311804f98
George Fekete <george@primalskill.com>
2025-05-03 18:43:11 +0300

    nvim - add close buffer (buffer delete)

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -19,8 +19,12 @@ map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keep current line in the center when moving up / down
 map('n', '<C-d>', '<C-d>zz')
 map('n', '<C-u>', '<C-u>zz')
+
+-- Buffers
+map('n', '<leader>q', '<CMD>bd<CR>', { desc = 'Close buffer' })
 
 -- ---------
 -- Filetypes
