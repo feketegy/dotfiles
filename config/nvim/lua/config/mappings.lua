c1768e674dec52e6630011c659eb86a10ada4afa
George Fekete <george@primalskill.com>
2025-05-03 19:57:33 +0300

    nvim - update mappings

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -1,15 +1,36 @@
 -- Keymaps and filetype mappings
 
--- -------
--- Keymaps
--- -------
+-- ------------------
+-- Utils for mappings
+-- ------------------
+
+-- Close floating windows
+local function close_floating()
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative == 'win' then
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+end
+
+-- Keymap shortcut
 local function map(mode, l, r, opts)
   opts = opts or {}
   vim.keymap.set(mode, l, r, opts)
 end
 
+-- -------
+-- Keymaps
+-- -------
+
 -- Clear highlights on search when pressing <Esc> in normal mode
-map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', function()
+  close_floating()
+  vim.cmd ':nohlsearch'
+end, {
+  silent = true,
+  desc = 'Remove search highlight and dismiss popups',
+})
 
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
