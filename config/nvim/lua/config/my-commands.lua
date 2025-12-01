0279a6aa66e4cea0d419698438c06f0701adfe8a
George Fekete <george@primalskill.com>
2025-12-01 16:53:37 +0200

    nvim: add custom command to remove items from the quickfix list

--- config/nvim/lua/config/my-commands.lua
+++ config/nvim/lua/config/my-commands.lua
@@ -14,3 +14,29 @@ cmd('TextYankPost', {
     }
   end,
 })
+
+cmd('FileType', {
+  desc = 'Remove a line from the quickfix list by using dd',
+  group = my_commands,
+  pattern = 'qf',
+  callback = function(event)
+    local bufmap = require('config.utils').bufmap
+
+    bufmap('dd', function()
+      local qflist = vim.fn.getqflist()
+      if #qflist == 0 then
+        return
+      end
+
+      local line = vim.fn.line '.'
+      table.remove(qflist, line)
+      vim.fn.setqflist({}, ' ', { items = qflist })
+
+      local new_count = #qflist
+      if new_count > 0 then
+        local new_line = math.min(line, new_count)
+        vim.api.nvim_win_set_cursor(0, { new_line, 0 })
+      end
+    end, 'Delete line in the quickfix list', event.buf, 'n')
+  end,
+})
