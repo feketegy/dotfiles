512d4e4b00fc2f1c1ad3aac9821831736b86e4df
George Fekete <george@primalskill.com>
2026-04-19 16:22:45 +0300

    nvim: add wip statusline, remove lualine

--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -1,4 +1,9 @@
-require("config.ui2")
-require("config.options")
-require("config.mappings")
-require("config.autocommands")
+require 'config.ui2'
+require 'config.options'
+require 'config.mappings'
+require 'config.autocommands'
+
+require("config.statusline")
+
+-- require 'plugins.lualine'
+
